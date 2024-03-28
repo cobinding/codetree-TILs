d9b18973fec2a3d41df4b7c9ea799a829787f003
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-03-29 02:16:27 +0900

    Title: 2진수로 변환하기, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240329/2진수로 변환하기/convert-to-binary.py
+++ 240329/2진수로 변환하기/convert-to-binary.py
@@ -1,16 +1,18 @@
 n=int(input())
 
-ans=str()
-while True:
-    if n < 1 : break
+if n==0: 
+    print(0)
+elif n==1:
+    print(1)
+else:
+    ans=str()
+    while True:
+        if n < 1 : break
 
-    if n%2==0:
-        ans+='0'
-    else:
-        ans+='1'
+        if n%2==0:
+            ans+='0'
+        else:
+            ans+='1'
 
-    n//=2
-
-if n==0: print(0)
-elif n==1: print(1)
-else: print(ans[::-1])
+        n//=2
+    print(ans[::-1])
