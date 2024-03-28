503cdc2b785583cf69a13193609fc7af00795736
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-03-29 01:43:23 +0900

    Title: Date to Date, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240329/Date to Date/date-to-date.py
+++ 240329/Date to Date/date-to-date.py
@@ -1,13 +1,18 @@
 m1, d1, m2, d2=map(int,input().split())
 
 # 각 코드마다 몇 일이 있는지를 배열로 저장
-num_of_days=[0,31,28,31,30,31,30,31,30,31,31,30,31,30,31]
+num_of_days=[0,31,28,31,30,31,30,31,31,30,31,30,31]
 
-start_day=num_of_days[m1]-d1
-end_day=d2
-ans=end_day+start_day+1
-
+ans=0
 if m2-m1 > 1:
-    ans += num_of_days[m1:m2+1]
+    ans += sum(num_of_days[m1+1:m2])
+    ans +=num_of_days[m1]-d1
+    ans += d2
+elif m2-m1 == 1:
+    start_day=num_of_days[m1]-d1
+    end_day=d2
+    ans=end_day+start_day
+elif m2-m1 == 0 :
+    ans=d2-d1
 
-print(ans)
+print(ans+1)
