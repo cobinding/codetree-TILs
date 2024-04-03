69c3823749a024750463a84af965c1e33cc0f94c
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-04-03 19:40:29 +0900

    Title: 가장 먼저 나오는 숫자, Time: 232ms, Memory: 34MB, Status: Passed - CodeTree

--- 240403/가장 먼저 나오는 숫자/first-appear-number.py
+++ 240403/가장 먼저 나오는 숫자/first-appear-number.py
@@ -3,16 +3,18 @@ arr=list(map(int,input().split()))
 search=list(map(int,input().split()))
 
 for target in search:
-    if target in set(arr):
-        left,right=0,n-1
-        lower_idx=n-1
-        while left<=right:
-            mid=(left+right)//2
-            if target<=arr[mid]:
-                lower_idx=min(lower_idx,mid)
-                right=mid-1
-            else:
-                left=mid+1
+    lower_idx=n
+    left,right=0,n-1
+    while left<=right:
+        mid=(left+right)//2
+        if target<=arr[mid]:
+            lower_idx=min(lower_idx,mid)
+            right=mid-1
+        else:
+            left=mid+1
+
+
+    if lower_idx+1 <= n and arr[lower_idx] == target:
         print(lower_idx+1)
     else:
         print(-1)
