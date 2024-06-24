6b9f4c0d56038d44647d577a7f85695f5aff4b0b
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-06-25 00:51:12 +0900

    Title: 가장 짧은 부분합, Time: 128ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240625/가장 짧은 부분합/shortest-subtotal.py
+++ 240625/가장 짧은 부분합/shortest-subtotal.py
@@ -3,13 +3,14 @@ arr=list(map(int,input().split()))
 
 left,right=0,0
 target=0
-ans=float('inf') # 원소 값의 최대는 10,000
+section=100000 # n은 1과 100,000 사이이므로 구간의 최대 길이는 100,000
 
 while True:
     target+=arr[right]
 
     while target>=s:
-        ans=min(right-left+1, ans)
+        # 가장 짧은 구간은?
+        section=min(right-left+1, section)
         
         # left pointer 조작을 통해 구간합 탐색
         target-=arr[left]
@@ -21,4 +22,4 @@ while True:
     
     right += 1
 
-print(ans)
+print(section)
