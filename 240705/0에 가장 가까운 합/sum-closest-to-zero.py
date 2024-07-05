86ee6f25436b19efe6d8a6d209030701acbb0768
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-07-05 17:01:09 +0900

    Title: 0에 가장 가까운 합, Time: 133ms, Memory: 30MB, Status: Wrong Answer - CodeTree

---
+++ 240705/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -0,0 +1,22 @@
+import sys;input=sys.stdin.readline
+
+n=int(input())
+arr=sorted(map(int,input().split()))
+
+start,end=0,n-1
+min_sum=sys.maxsize
+
+while start < end and start <= 0:
+     
+    sum_numbers=arr[start]+arr[end]
+    min_sum=min(abs(sum_numbers), min_sum)
+
+    if sum_numbers > 0 :
+        end -= 1
+    elif sum_numbers < 0 :
+        start += 1
+    else: break
+    
+    
+
+print(abs(min_sum))
