96a8be418d32bce6f07738139ba1247eec4553aa
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-07-11 17:31:17 +0900

    Title: 최소공배수 구하기, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240711/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240711/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,13 +1,13 @@
 n,m=map(int,input().split())
 
-
 def get_lcm(n,m):
     lcm=0
     # 최소공배수는 두 수의 곱보다 작을 수 없음
     for i in range(max(n,m), n*m+1):
         # 현재 값이 n과 m의 공통 배수인지 확인
-        if i%n==0 and i%m==0
+        if i%n==0 and i%m==0:
             lcm=i
+            break
     
     return lcm
 
