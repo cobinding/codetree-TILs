2569b3e80d30cfdfd50900776a3897937a5da87d
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-07-11 10:06:26 +0900

    Title: 최대공약수 구하기, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240711/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240711/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,9 +1,8 @@
 n,m=map(int,input().split())
 
-ans=0
-for i in range(min(n,m), 1, -1):
+gcd=0
+for i in range(1,min(n,m)+1):
     if n%i == 0 and m%i == 0:
         ans=i
-        break
 
 print(ans)
