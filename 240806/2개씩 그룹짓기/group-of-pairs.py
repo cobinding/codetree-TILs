33bc23cb5c8623ffd19886e5dad93ea460f51f18
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-08-06 22:04:33 +0900

    Title: 2개씩 그룹짓기, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240806/2개씩 그룹짓기/group-of-pairs.py
+++ 240806/2개씩 그룹짓기/group-of-pairs.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = sorted(map(int, input().split()))
 
 group_max = 0
-for i in range(n):
+for i in range(2*n):
     # 양 끝쪽에서부터 덧셈하기
     max_sum = arr[i] + arr[n*2-i-1]
     if max_sum > group_max:
