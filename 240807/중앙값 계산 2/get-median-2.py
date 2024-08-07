ea4d0a2d1eab53371c244e3ab3f1f4ba8249ab1f
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-08-07 14:00:03 +0900

    Title: 중앙값 계산 2, Time: 155ms, Memory: 73MB, Status: Passed - CodeTree

--- 240807/중앙값 계산 2/get-median-2.py
+++ 240807/중앙값 계산 2/get-median-2.py
@@ -2,9 +2,9 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 for i in range(n):
-
     # 홀수 번째 수 == 인덱스가 짝수인 것
     if i == 0:
         print(arr[i], end = " ")
     elif i % 2 == 0:
-        print(arr[i//2], end = " ")
+        set_arr = sorted(arr[:i+1])
+        print(set_arr[i//2], end = " ")
