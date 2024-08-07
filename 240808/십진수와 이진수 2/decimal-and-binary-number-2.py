203449a4423a6455ce5e9ffc1fb5c42bed04736e
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-08-08 00:59:36 +0900

    Title: 십진수와 이진수 2, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240808/십진수와 이진수 2/decimal-and-binary-number-2.py
+++ 240808/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -1,4 +1,30 @@
+# 십진수로 변겅
+def to_decimal(binary_number):
+    number = 0
+    n = len(binary_number)-1
+    for i in range(n, -1, -1):
+        number += 2**(n-i) * binary_number[i]
+    
+    return number
+
+# 이진수로 변경
+def to_binary(decimal_number):
+    binary_list = []
+    while decimal_number != 0:
+        binary_list.append(decimal_number%2)
+        decimal_number //= 2
+
+    # 연산한 값을 reverse하여 이진수 도출
+    re_binary_list = []
+    for i in range(len(binary_list)-1, -1, -1):
+        re_binary_list.append(binary_list[i])
+    
+    return re_binary_list
+
+
 binary_n = input()
-n = int(binary_n, 2)*17
-re_binary_n = bin(n)
-print(re_binary_n[2:])
+binary_n = list(map(int, binary_n))
+decimal_number = to_decimal(binary_n) * 17
+# 17배 이후 다시 이진수로
+ans = to_binary(decimal_number)
+print(''.join(map(str,ans)))
