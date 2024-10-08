d5ab1878f8df62a525a08859af43c464227da99d
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-10-08 15:16:33 +0900

    Title: 크기가 n인 순열, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241008/크기가 n인 순열/n-permutation.py
+++ 241008/크기가 n인 순열/n-permutation.py
@@ -1,29 +1,32 @@
 import sys; input=sys.stdin.readline
 
-n = int(input())
+n = int(input().rstrip())
 visited = [False] * (n+1)
 answer = []
 
+# 중복없는 순열
 def permutations(curr_num):
     if curr_num == n+1 :
-        print(*answer)
+        print(answer)
         return
 
     # 1부터 n까지 순열을 만드는 과정
     for i in range(1, n+1):
-
         if visited[i] :
             continue
 
         # 사용한 숫자에 대한 방문 처리
         visited[i] = True
         answer.append(i)
-
-        # 순서 보장을 위해 1씩 늘려가면서 함수 호출
+        # print("append, ", answer, visited )
+        # 순서 보장을 위해 자릿수 1씩 늘려가면서 함수 호출
         permutations(curr_num+1)
 
-        # 재귀함수가 return된 숫자에 대해서는 pop을 함으로써 배열의 끝 부분 제거
+        # 조건에 해당할 때까지 append 수행
+        # return 조건에 다다르면 호출된 재귀스택들이 return 하면서 pop
+        # 방문 처리 && 숫자 만들기까지 끝내고 return 됨
         answer.pop()
+        # print("pop", answer, visited)
         visited[i] = False
 
 permutations(1)
