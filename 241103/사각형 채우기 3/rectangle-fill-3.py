e987ace0a97e3584f431ca4b45432de8a7f54c2b
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-11-03 12:30:52 +0900

    Title: 사각형 채우기 3, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241103/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,15 @@
+n = int(input())
+dp = [0 for _ in range(n+2)]
+
+dp[1] = 2
+dp[2] = 7
+dp_half[1] = 1
+dp_half[2] = 3
+
+
+for i in range(3, n+1):
+    # half 두개 세로로 겹치는 경우는 dp에서 측정
+    dp[i] = dp[i-1]*2 + dp[i-2] + dp_half[i-1]*2
+    dp_half[i] = dp[i-1] + dp_half[i-1]
+
+print(dp[n]%1000000007)
