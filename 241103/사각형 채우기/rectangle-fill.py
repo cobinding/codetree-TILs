0c039b8b959c9f926c0985a759248d23a154b47f
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-11-03 11:55:46 +0900

    Title: 사각형 채우기, Time: 50ms, Memory: 15MB, Status: Passed - CodeTree

--- 241103/사각형 채우기/rectangle-fill.py
+++ 241103/사각형 채우기/rectangle-fill.py
@@ -1,5 +1,4 @@
 n = int(input())
-
 dp = [0 for _ in range(n+2)]
 
 dp[0] = 0
@@ -9,4 +8,4 @@ dp[2] = 2
 for i in range(3, n+1):
     dp[i] = dp[i-1] + dp[i-2]
 
-print(dp[n+1] % 10007)
+print(dp[n] % 10007)
